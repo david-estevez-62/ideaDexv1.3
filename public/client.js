0e5f1fe21741625efcae73b3324c0c638ddb911c
Eric <ejh81491@gmail.com>
2015-12-14 14:33:59 -0500

    sign up with email and email verification | passport facebook and google signin

--- public/client.js
+++ public/client.js
@@ -5,42 +5,107 @@ $(document).ready(function(){
 	// };
 
 
+
 ////////////////////////////
 // SignUp Page Validation //
 ////////////////////////////
 
-	// $('.createacct').on('click', function(e){
-	// 	e.preventDefault();
+	$('.createacct').on('click', function(e){
+		e.preventDefault();
 
-	// 	console.log('hi');
+		var username = $('.loginusername').val();
+		console.log(username)
 
-	// 	var username = $('.loginusername').val();
+		var password1 = $('.loginpassword1').val();
+		var password2 = $('.loginpassword2').val();
 
-	// 	var password1 = $('.loginpassword1').val();
-	// 	var password2 = $('.loginpassword2').val();
+		if (password1 === password2) {
+			document.getElementById("createacct").submit();
+		} else {
+			console.log('do nothing')
+		}
 
-	// 	if(password1 === password2){
-
-	// 		$.post('/signup', {username:username, password1:password1} , function(data){
-
-	// 			console.log(data)
-					
-	// 		});
-
-	// 		$('.loginusername').val('');
-	// 		$('.loginpassword1').val('');
-	// 		$('.loginpassword2').val('');
-
-	// 	}
-	// 	else {
-	// 		$('#signup').append('<p>passwords dont match up.</p>')
-	// 	}
-
-
-	// })
+	})
 
 
 
+////////////////////////////
+// Password Typing status //
+////////////////////////////
+		// $('.loginpassword1').keyup(function() {
+		// 	console.log($(this).context.value.length)
+		// 	if(Number($(this).context.value.length) > 0){
+		// 		$('.status')[0].innerHTML = "typing...";
+		// 	} else if(Number($(this).context.value.length) == "") {
+		// 		$('.status')[0].innerHTML = "Passwords are empty";
+		// 	}
+		// });
+		// $('.loginpassword2').keyup(function() {
+		// 	if(Number($(this).context.value.length) > 0){
+		// 		$('.status')[0].innerHTML = "typing...";
+		// 	} else if(Number($(this).context.value.length) == "") {
+		// 		$('.status')[0].innerHTML = "Password are empty";
+		// 	}
+		// });
+
+		$('.loginpassword1').keyup(function() {
+			var password1 = $('.loginpassword1').val();
+			var password2 = $('.loginpassword2').val();
+			console.log(password1)
+
+			if(Number($(this).context.value.length) > 0  && password1 === password2){
+				$('.status')[0].innerHTML = "Password match";
+
+
+			} else if (Number($(this).context.value.length) > 0) {
+				$('.status')[0].innerHTML = "Passwords don't match";
+
+				console.log('btn is not clickable until ')
+			}
+		});
+
+		$('.loginpassword2').keyup(function() {
+			var password1 = $('.loginpassword1').val();
+			var password2 = $('.loginpassword2').val();
+			console.log(password1)
+
+			if(Number($(this).context.value.length) > 0  && password1 === password2){
+				$('.status')[0].innerHTML = "Password match";
+
+
+			} else if (Number($(this).context.value.length) > 0) {
+				$('.status')[0].innerHTML = "Passwords don't match";
+
+				console.log('btn is not clickable until ')
+			}
+		});
+
+
+// show/hide(toggle) password
+function togglePassword() {
+	
+
+	var pw1 = document.getElementsByClassName('loginpassword1');
+	var pw2 = document.getElementsByClassName('loginpassword2');
+
+	console.log(pw1)
+
+	var togglePw = document.getElementById('togglePw');
+	if(pw1[0].type == "password"){
+		pw1[0].type = "text";
+		pw2[0].type = "text";
+		togglePw.value = "Hide pw";
+	} else {
+		console.log('hi')
+		pw1[0].type = "password";
+		pw2[0].type = "password";
+		togglePw.value = "Show pw";
+	}
+}
+
+$("#togglePw").on('click', function(){
+	togglePassword()
+})
 
 
 
